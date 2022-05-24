c5f58248e16b09230f9329a0de6fffba0abab0eb
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-05-24 06:43:59 +0200

    change the domain origin

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://kemoday.github.io",
     credentials: true,
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://kemoday.github.io");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
