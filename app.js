4263e07c7d14409fb7501a986f389b88950552dc
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-05-25 05:36:37 +0200

    smiplfiy cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://kemoday.github.io/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
